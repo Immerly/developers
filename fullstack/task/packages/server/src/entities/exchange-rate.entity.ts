726c2ee6a7a49f8dcac40ac9250965067ce5370e
giogiga <126618424+giogiga@users.noreply.github.com>
2023-10-25 00:45:57 +0200

    feat: add exchange-rate entity

---
+++ fullstack/task/packages/server/src/entities/exchange-rate.entity.ts
@@ -0,0 +1,39 @@
+import { Field, ObjectType } from '@nestjs/graphql';
+import { IsNumber, IsString } from 'class-validator';
+import { Column, Entity } from 'typeorm';
+import { EntityWithMeta } from '../common';
+import { VAR_CHAR } from './constants';
+
+@ObjectType()
+@Entity()
+export class ExchangeRate extends EntityWithMeta {
+  @IsString()
+  @Field(() => String)
+  @Column({ type: 'varchar', length: 10 })
+  public validFor!: string;
+
+  @IsNumber()
+  @Field(() => Number)
+  @Column({ type: 'int' })
+  public order!: number;
+
+  @IsString()
+  @Field(() => String)
+  @Column({ ...VAR_CHAR })
+  public country!: string;
+
+  @IsString()
+  @Field(() => String)
+  @Column({ ...VAR_CHAR })
+  public currency!: string;
+
+  @IsString()
+  @Field(() => String)
+  @Column({ type: 'varchar', length: 3 })
+  public currencyCode!: string;
+
+  @IsNumber()
+  @Field(() => Number)
+  @Column({ type: 'float' })
+  public rate!: number;
+}
---
+++ fullstack/task/packages/server/src/migrations/1698187508828-add-exchange-rate.ts
@@ -0,0 +1,16 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class addExchangeRate1698187508828 implements MigrationInterface {
+    name = 'addExchangeRate1698187508828'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "example" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "createdAtUtc" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "updatedAtUtc" TIMESTAMP WITH TIME ZONE DEFAULT now(), "deleteDateUtc" TIMESTAMP WITH TIME ZONE, "version" integer NOT NULL, "name" character varying(255) NOT NULL, "value" character varying(255) NOT NULL, CONSTRAINT "PK_608dd5fd6f0783062b07346ed1c" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "exchange_rate" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "createdAtUtc" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "updatedAtUtc" TIMESTAMP WITH TIME ZONE DEFAULT now(), "deleteDateUtc" TIMESTAMP WITH TIME ZONE, "version" integer NOT NULL, "validFor" character varying(10) NOT NULL, "order" integer NOT NULL, "country" character varying(255) NOT NULL, "currency" character varying(255) NOT NULL, "currencyCode" character varying(3) NOT NULL, "rate" double precision NOT NULL, CONSTRAINT "PK_5c5d27d2b900ef6cdeef0398472" PRIMARY KEY ("id"))`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "exchange_rate"`);
+        await queryRunner.query(`DROP TABLE "example"`);
+    }
+
+}
