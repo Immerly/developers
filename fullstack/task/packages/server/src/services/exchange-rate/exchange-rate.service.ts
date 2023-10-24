f1575e3d056d4e6108b7a490817297fdcfa5f1f6
giogiga <126618424+giogiga@users.noreply.github.com>
2023-10-25 01:21:46 +0200

    feat: add function to fetch exchange rates

--- fullstack/task/packages/server/src/services/exchange-rate/exchange-rate.service.ts
+++ fullstack/task/packages/server/src/services/exchange-rate/exchange-rate.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { z } from 'zod';
 import { ExchangeRate } from '../../entities';
 
 @Injectable()
@@ -10,7 +11,8 @@ export class ExchangeRateService {
     ) { }
 
     public getExchangeRates = async () => {
-        return [];
+        const exchangeRates = await this.fetchExchangeRates();
+        return exchangeRates;
     };
 
     private async refreshExchangeRates() {
@@ -18,7 +20,37 @@ export class ExchangeRateService {
     }
 
     private async fetchExchangeRates() {
-        // Do stuff
+        const res = await fetch('https://api.cnb.cz/cnbapi/exrates/daily?lang=EN').catch((err) => {
+            console.error("Couldn't fetch API:", err);
+        });
+        if (!res || (res && !res.ok)) return [];
+
+        const data = await res.json().catch((err) => {
+            console.error("Couldn't parse JSON:", err);
+        });
+
+        const RawExchangeRate = z.object({
+            validFor: z.string(),
+            order: z.number(),
+            country: z.string(),
+            currency: z.string(),
+            amount: z.number(),
+            currencyCode: z.string(),
+            rate: z.number(),
+        });
+
+        const RawRatesResponse = z.object({
+            rates: z.array(RawExchangeRate),
+        });
+
+        const parsedData = RawRatesResponse.safeParse(data);
+
+        if (!parsedData.success) {
+            console.error(parsedData.error);
+            return [];
+        }
+
+        return parsedData.data.rates;
     }
 
     private async getCachedRates() {
