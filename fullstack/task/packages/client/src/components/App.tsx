ff10034a0b13983b4d83ff49137890a717cef8db
Jon Ribeiro <contact@jonathas.com>
2023-12-07 16:52:51 +0100

    perf: removed duplicate useQuery

--- fullstack/task/packages/client/src/components/App.tsx
+++ fullstack/task/packages/client/src/components/App.tsx
@@ -1,14 +1,7 @@
 import React from 'react';
-import { useQuery } from '@apollo/client';
-import { GET_EXCHANGE_RATES } from '../queries';
 import ExchangeRates from './ExchangeRates';
 
 const App: React.FC = () => {
-    const { loading, error } = useQuery(GET_EXCHANGE_RATES);
-
-    if (loading) return <p>Loading...</p>;
-    if (error) return <p>Error: {error.message}</p>;
-
     return (
         <div className="container mx-auto p-4">
             <h1 className="text-2xl font-bold mb-4">Currency Exchange App</h1>
