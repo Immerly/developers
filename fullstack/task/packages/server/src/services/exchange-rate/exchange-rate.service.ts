e26e87e0a563441c06a2aaf5c8de130b71319970
kolatajp <pawel@kolataj.pl>
2023-12-10 23:36:48 +0100

    introduced fetch bank page html method

--- fullstack/task/packages/server/src/services/exchange-rate/exchange-rate.service.ts
+++ fullstack/task/packages/server/src/services/exchange-rate/exchange-rate.service.ts
@@ -1,7 +1,17 @@
+import { ConfigService } from '@nestjs/config';
 import { Injectable } from '@nestjs/common';
+import axios from 'axios';
 
 @Injectable()
 export class ExchangeRateService {
+    constructor(private readonly configService: ConfigService) {}
+
+    public fetchBankPageHTML = async (): Promise<string> => {
+        const bankURL = this.configService.get<string>('EXCHANGE_RATE_BANK_URL');
+        const response = await axios.get(bankURL!);
+        return response.data;
+    };
+
     public getExchangeRates = async () => {
         // TODO: Implement the fetching and parsing of the exchange rates.
         // Use this method in the resolver.
